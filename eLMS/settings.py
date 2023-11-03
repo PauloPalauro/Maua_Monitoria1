fe6fe7cfe859a8bb92e71f1c1637c4f438509bcd
PauloEduardoPalauro <paulopalauro@gmail.com>
2023-11-03 14:51:54 -0300

    db

--- eLMS/settings.py
+++ eLMS/settings.py
@@ -139,6 +139,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
