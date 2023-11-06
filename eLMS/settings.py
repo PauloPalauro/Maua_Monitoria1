9da6763303cd226102a29c84f7b312ce0b9f46e8
PauloEduardoPalauro <paulopalauro@gmail.com>
2023-11-06 10:13:30 -0300

    test vercel

--- eLMS/settings.py
+++ eLMS/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-_@876m&g2$*55!90p5cvqfsb)_f07n#33vhp2^3ggabcx#zyjr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
